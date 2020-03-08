8009886188fa24a39d0f80f718c5eb3368ca593e
Julio <julio.augmi@hotmail.com>
2020-03-08 03:29:26 -0300

    att antes de dormir ne

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <title>Teste da SKY</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 </head>
 <body>
 
@@ -45,53 +44,53 @@
 
   <br>
 
-  <div class="container"> 
-    <div class="row-2"> 
-      <button type="button" class="btn btn-secondary btn-lg" onclick="Mudar_Estado('ajax_form')">Cadastrar nova despesa</button>
-    </div>
+  <div align="center"> 
+      <button type="button" class="btn btn-secondary btn-lg" id="btnExibir">Cadastrar nova despesa</button>
   </div>
 
   <br>
 
-  <form method="post" action="" id="ajax_form">
-    <div class="form row col-md-12"> 
+  <div class="container">
+    <form method="post" action="" id="ajax_form">
+      <div class="form row"> 
 
-      <div class="form-group col-md-4">
-        <input type="text" class="form-control" name="nome" placeholder="Nome">
+        <div class="form-group col-md-4 col-xs-12">
+          <input type="text" class="form-control" name="nome" placeholder="Nome">
+        </div>
+
+        <div class="form-group col-md-4 col-xs-12">
+          <select class="form-control" name="categoria" placeholder="Categoria">
+            <option disabled selected style="display: none;">Categoria</option>
+            <option value="Alimentação">Alimentação</option>
+            <option value="Saúde">Saúde</option>
+            <option value="Transporte">Transporte</option>
+            <option value="Entretenimento">Entretenimento</option>
+            <option value="Casa">Casa</option>
+          </select>
+        </div>
+
+        <div class="form-group col-md-4 col-xs-12">
+          <input type="text" class="form-control" name="valor" placeholder="Valor (R$)">
+        </div>
+
       </div>
 
-      <div class="form-group col-md-4">
-        <select class="form-control" name="categoria" placeholder="Categoria">
-          <option disabled selected style="display: none;">Categoria</option>
-          <option value="Alimentação">Alimentação</option>
-          <option value="Saúde">Saúde</option>
-          <option value="Transporte">Transporte</option>
-          <option value="Entretenimento">Entretenimento</option>
-          <option value="Casa">Casa</option>
-        </select>
+      <div align="center">
+        <button type="submit" class="btn btn-secondary btn-lg" name="enviar">Cadastrar</button>
       </div>
 
-      <div class="form-group col-md-4">
-        <input type="text" class="form-control" name="valor" placeholder="Valor (R$)">
-      </div>
-
-    </div>
-
-    <div align="center">
-      <button type="submit" class="btn btn-secondary btn-lg" name="enviar">Cadastrar</button>
-    </div>
-
-    <!-- <label><input id = "btn" type="submit" name="enviar" value="Enviar" /></label> -->
-
-  </form>
-
+    </form>
+  </div>
 </body>
 </html>
 
-<script type="text/javascript">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>
 
   jQuery(document).ready(function(){
-    jQuery('#ajax_form').submit(function(){
+
+    jQuery('#ajax_form').submit(function(e){
+      e.preventDefault();
       var dados = jQuery(this).serialize();
 
       jQuery.ajax({
@@ -100,19 +99,35 @@
         data: dados,
         success: function(data)
         {
-          console.log(data);
+          console.log("passei aq");
         }
       });
       return false;
     });
+    
   });
 
-  function Mudar_Estado(elmt) {
-        var display = document.getElementById(elmt).style.display;
-        if(display == "none")
-            document.getElementById(elmt).style.display = 'block';
-        else
-            document.getElementById(elmt).style.display = 'none';
+  const buttomExibe = document.querySelector("#btnExibir");
+
+  buttomExibe.addEventListener('click', function(e){
+    e.preventDefault() //Previne a ação padrão do elemento
+
+    let form = document.querySelector("#ajax_form");
+    if (form.style.display != "block"){
+      form.setAttribute("style", "display: block;");
+    }else{
+      form.setAttribute("style", "display: none;");
     }
+    console.log(form.style);
+  })
+
+  // function mudarEstado(elmt) {
+  //       var display = document.querySelector(elmt).style.display;
+  //       console.log(display);
+  //       if(display == "none")
+  //           document.querySelector(elmt).style.display = 'block';
+  //       else if(display == "block")
+  //           document.querySelector(elmt).style.display = 'none';
+  //   }
 
 </script>
